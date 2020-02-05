1702dd6ab9a405c85bbd03b2cf3915cb412b2ad2
JuanSebastianC9 <46875830+JuanSebastianC9@users.noreply.github.com>
2020-02-04 21:09:54 -0500

    Update RedundantConnection

--- 312/312. Burst Balloons.java
+++ 312/312. Burst Balloons.java
@@ -1,8 +1,27 @@
+import java.util.Arrays;
 class Solution {
     public int maxCoins(int[] nums) {
-        int locMax = 0;
-        for(int i = 0; i < nums.length; i++){
-            
+        int addNum = 0;
+        int[] newNums = {};
+        if(nums.length == 1){
+            addNum = nums[0];
+            return addNum;
         }
+        else if(nums.length == 2){
+            addNums = (addNums[0] * addNums[1]);
+            return addNum + maxCoins(newNums);
+        }
+        else {
+            int minLoc = 0;
+            for(int i = 0; i < nums.length; i++){
+                if(nums[minLoc] > nums[i]){
+                    minLoc = i;
+            }
+            if(minLoc == 0){
+               addNum = nums[0] 
+            }
+            
+            return addNum + maxCoins(newNums);
+        } 
     }
 }
---
+++ 684/RedundantConnection.java
@@ -0,0 +1,13 @@
+public class RedundantConnection{
+    public int[] findRedundantConnection(int[][] edges){
+        
+        int[] adjVertices = new int[matrix[v].length];
+        int size = 0;
+
+        for(int i = 0; i < edges.length; i++){
+
+        }
+
+          return new int[0];
+    }
+}
